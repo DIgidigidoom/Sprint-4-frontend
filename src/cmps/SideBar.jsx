c081c3b56ecfd6095b83cfd961743a47e8e13f51
bnethanel <bnethanel@gmail.com>
2025-05-12 13:00:38 +0300

    improve sidebar scss

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export function SideBar() {
-  return (
-    <div className='sidebar'>SideBar</div>
-  )
+    return (
+        <div className='sidebar'>
+            <div className='sidebar-header'>
+                
+                    <span>Your Library</span>
+                
+                <button className='sidebar-create-btn'> <FontAwesomeIcon icon={faPlus} className='sidebar-plus' />Create</button>
+            </div>
+
+        </div>
+    )
 }
