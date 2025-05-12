b5c581d5315039cf6019a3555c8e1a2ffc04d1e7
hadar <hadarsabag5@gmail.com>
2025-05-12 13:54:17 +0300

    Changed create station location

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
+import { stationService } from '../services/station/index.js'
+import { addStation } from '../store/actions/station.actions.js'
+import { useNavigate } from 'react-router-dom'
 
 export function SideBar() {
+
+    const navigate = useNavigate()
+
+    async function onCreateStation() {
+        const station = stationService.getEmptyStation()
+        try {
+            const savedStation = await addStation(station)
+            navigate(`/station/${savedStation._id}`)
+        } catch (err) {
+            showErrorMsg('Cannot add station')
+            console.log("err: ", err)
+        }
+    }
+
     return (
         <div className='sidebar'>
             <div className='sidebar-header'>
-                
-                    <span>Your Library</span>
-                
-                <button className='sidebar-create-btn'> <FontAwesomeIcon icon={faPlus} className='sidebar-plus' />Create</button>
+
+                <span>Your Library</span>
+
+                <button onClick={() => onCreateStation()} className='sidebar-create-btn'> <FontAwesomeIcon icon={faPlus} className='sidebar-plus' />Create</button>
             </div>
 
         </div>
--- src/pages/StationIndex.jsx
+++ src/pages/StationIndex.jsx
@@ -10,6 +10,7 @@ import { SideBar } from '../cmps/SideBar.jsx'
 import { AppHeader } from '../cmps/AppHeader.jsx'
 import MainPage from './MainPage.jsx'
 import { EditStationModal } from '../cmps/EditStationModal.jsx'
+import { MediaPlayer } from '../cmps/MediaPlayer.jsx'
 
 export function StationIndex() {
 
@@ -38,29 +39,6 @@ export function StationIndex() {
         setStationToEdit(station)
     }
 
-    async function onCreateStation() {
-        const station = stationService.getEmptyStation()
-        try {
-            const savedStation = await addStation(station)
-            navigate(`/station/${savedStation._id}`)
-        } catch (err) {
-            showErrorMsg('Cannot add station')
-        }
-    }
-
-    async function onUpdateStation(station) {
-        station.title = prompt('Title?', 'Some Title')
-        const stationToSave = { ...station, name }
-        try {
-            const savedStation = await updateStation(stationToSave)
-            showSuccessMsg(`Station updated, new name: ${savedStation.name}`)
-        } catch (err) {
-            showErrorMsg('Cannot update station')
-        }
-        console.error('Cannot create station', err)
-        showErrorMsg('Cannot create station')
-    }
-
     return (
         <main className="station-index">
             <AppHeader />
@@ -74,6 +52,8 @@ export function StationIndex() {
                 onUpdateStation={onUpdateStation} />
 
             <SideBar />
+
+            <MediaPlayer />
 
             {stationToEdit && (
                 <EditStationModal
